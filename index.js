2dc01582be3e972ef79d890c1b1d9eb7b876c17b
Anthoniselvi <anthoniselvi33@gmail.com>
2022-09-01 19:07:56 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const slides = document.getElementsByClassName ('wonders-list');
+let slidePosition = 0;
+const totalSlides = slides.length;
+
+document.getElementById ('next-button').addEventListener ('click', moveToNextSlide);
+document.getElementById ('prev-button').addEventListener ('click', moveToPrevSlide);
+
+function hideAllSlides() {
+    for (let slide of slides){
+        slide.classList.remove ('wonders-list-visible');
+        slide.classList.add ('wonders-list-hidden');
+    }
+}
+
+function moveToNextSlide() {
+    hideAllSlides();
+
+    if (slidePosition === totalSlides - 1){
+    slidePosition = 0;
+    } else {
+    slidePosition ++;
+    }
+
+    slides[slidePosition].classList.add("wonders-list-visible");
+}
+
+function moveToPrevSlide() {
+    hideAllSlides();
+
+    if (slidePosition === 0){
+    slidePosition = totalSlides - 1;
+    } else {
+    slidePosition--;
+    }
+
+    slides[slidePosition].classList.add("wonders-list-visible");
+}
